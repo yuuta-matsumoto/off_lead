4422bce8d2d1ca9745420fd4824932c301db5615
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-01 17:42:39 +0900

    Create devise views

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module OffLead
   class Application < Rails::Application
+    config.i18n.default_locale = :ja # 追加
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
