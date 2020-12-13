ca2451997b90c46bdaea3b53c0fd9175f0a0e74f
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-13 12:36:48 +0900

    createアクションを作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+
   def new
+    @review = Review.new
   end
+
+  def create
+    @review = Review.new(params_review)
+    @review.user_id = current_user.user_id
+    if @review.save
+      flash[:success] = "口コミを投稿しました"
+      redirect_to post_path
+    else
+      render action: :new
+    end
+  end
+  private
+    def review_params
+      params.require(:review).permit(:title, :content).merge(user_id: current_user.id)
+    end
 end
