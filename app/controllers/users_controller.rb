52bd0225a02dd7a6bc8585ed15aa4289126697d9
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-15 17:01:27 +0900

    usersのshowアクションを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    @currentUserEntry = Entry.where(user_id: current_user.id) #ボタンを押したユーザーを探す
+    @userEntry = Entry.where(user_id: @user.id) #ボタンを押されたユーザーを探す。
+    unless @user.id == current_user.id #現在ログインしているユーザーではない
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then #ルームがすでに作成されている場合
+            @isRoom = true #false(roomが未作成)の時にroomを作成する条件を記述するため
+            @roomId = cu.room_id
+          end
+        end
+      end
+
+      if @isRoom #true（すでにroomがある）なら何も起きない
+      else #false(room)が未作成なら新しいインスタンスを作成するためのnewを記述
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
+
 end
