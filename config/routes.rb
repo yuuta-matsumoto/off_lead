592176b65c43c43d509d63b5e3efe15b1b323b9e
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-15 16:34:24 +0900

    ルーティング を設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   resources :users, :only => [:index, :show] 
-  
+  #メッセージ
+  resources :messages, :only => [:create]
+  #トークルーム
+  resources :rooms, :only => [:create, :show]
   root 'pages#index'
   get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
