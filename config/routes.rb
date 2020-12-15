dfe5fe95763575588c93790596cc5c5af86fca5d
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-15 09:22:05 +0900

    コントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  #レビュー
   resources :reviews, :only => [:create]
+  #投稿
   resources :posts
+  #ユーザー
   devise_for :users
   devise_for :views
+  get 'users/index'
+  get 'users/show'
+  
   root 'pages#index'
   get 'pages/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
