27daffe5d199a1febdfd04079668dd77d3a05404
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-16 23:34:50 +0900

    メッセージ機能実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :content])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :content])
     end
 end
