f68340864fe2613513916083ee73d623550cb036
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-21 20:55:24 +0900

    星評価実装完了

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :post, optional: true
+
+  validates :title, presence: true
+
+  validates :content, presence: true
+
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 0.5
+  }, presence: true
 end
