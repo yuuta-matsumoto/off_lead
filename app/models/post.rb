f16931902cce18679be121777256012870497843
yuuta-matsumoto <y.matsumo73@gmail.com>
2020-12-23 17:38:28 +0900

    検索窓を表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   mount_uploader :img, ImgUploader
   has_many :likes, dependent: :destroy
   has_many :liked_uses, through: :likes, source: :user #post.liked_usersでpostのいいね一覧を取り出せる。
+
+  def self.search(search)
+    if search
+      Post.where(['content LIKE ?', "%#{search}%"])
+    else
+      Post.all
+    end
+  end
 end
