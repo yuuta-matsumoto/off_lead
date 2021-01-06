da1561cf8e19e5f77072493595a1deb14fe063b7
yuuta-matsumoto <y.matsumo73@gmail.com>
2021-01-06 17:53:31 +0900

    アイコンを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permitted_parameters
+      added_attrs = [:name, :email, :password, :img]
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :content])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :content])
     end
