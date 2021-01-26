9ab9b0dfcce845d91ae95f3f176361f3f8c44065
yuuta-matsumoto <y.matsumo73@gmail.com>
2021-01-26 17:07:35 +0900

    環境変数を設定

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+
 
 module OffLead
   class Application < Rails::Application
