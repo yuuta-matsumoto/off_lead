e4195560099a1144d1198ad92d5bf0d5867627c1
yuuta-matsumoto <y.matsumo73@gmail.com>
2021-01-26 13:58:18 +0900

    database.ymlを変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,9 @@ set :deploy_to, '/var/www/rails/off_lead'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+#master.keyを読み込ませる
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.3'
 set :rbenv_path, '/home/yuta/.rbenv'
