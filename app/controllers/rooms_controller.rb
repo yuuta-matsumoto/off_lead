8e7f84d6fcc7d51c025f2e9e39cd325014e0bf87
yuuta-matsumoto <y.matsumo73@gmail.com>
2021-01-30 16:36:56 +0900

    メッセージ画面を整える

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,7 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  add_breadcrumb "ホーム" , :root_path
+  add_breadcrumb 'ユーザー一覧', :users_path
 
   def create
     @room = Room.create(user_id: current_user.id)#現在ログインしているユーザーのentry
@@ -16,8 +18,16 @@ class RoomsController < ApplicationController
       @messages = @room.messages #@roomと紐づいたメッセージを表示する
       @message = Message.new #messageのインスタンスを作成するため
       @entries = @room.entries #ユーザーの名前などの情報を表示するため
+      #パンクズで相手ユーザー名を表示する
+      @entries.each do |e|
+        unless e.user.name == current_user.name
+          add_breadcrumb "#{e.user.name}", "/users/#{e.user.id}"
+        end
+      end
     else
       redirect_back(fallback_location: root_path) #前のページに戻す
     end
+      add_breadcrumb "メッセージ", :room_path
   end
+
 end
