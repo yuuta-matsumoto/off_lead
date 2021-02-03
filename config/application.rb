09031bf4b44b1a0d20dcfe9ef044af7f94d04a80
yuuta-matsumoto <y.matsumo73@gmail.com>
2021-02-04 08:24:57 +0900

    git clone を実行

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Dotenv::Railtie.load
 module OffLead
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.i18n.default_locale = :ja # 日本語化
     config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
